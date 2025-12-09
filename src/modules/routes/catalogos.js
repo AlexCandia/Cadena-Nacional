30f5800f942ff6d1d843da16f6f9b39642e22593
alex <202300124@est.umss.edu>
2025-12-08 21:42:35 -0400

    Feat:protected routes

--- src/modules/routes/catalogos.js
+++ src/modules/routes/catalogos.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 const catalogoController = require('../controllers/catalogosController');
-
-router.post('/', catalogoController.createCatalogo);
+router.use(auth.isAuthenticated);
+router.post('/',auth.checkRole('administrador'), catalogoController.createCatalogo);
 router.get('/', catalogoController.getCatalogos);
-router.put('/:id', catalogoController.updateCatalogo);
-router.delete('/:id', catalogoController.deleteCatalogo); 
+router.put('/:id',auth.checkRole('administrador'), catalogoController.updateCatalogo);
+router.delete('/:id',auth.checkRole('administrador'), catalogoController.deleteCatalogo); 
 
 module.exports = router;
--- src/modules/routes/subareas.js
+++ src/modules/routes/subareas.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 const subareaController = require('../controllers/subareasController');
-
-router.post('/', subareaController.createSubarea);
+router.use(auth.isAuthenticated);
+router.post('/',auth.checkRole('administrador', 'profesor'), subareaController.createSubarea);
 router.get('/', subareaController.getSubareas);
 router.get('/:id', subareaController.getSubareaById);
 router.get('/byArea/:areaId', subareaController.getSubareasByArea);
-router.put('/:id', subareaController.updateSubarea);
-router.delete('/:id', subareaController.deleteSubarea);
+router.put('/:id',auth.checkRole('administrador', 'profesor'), subareaController.updateSubarea);
+router.delete('/:id',auth.checkRole('administrador', 'profesor'), subareaController.deleteSubarea);
 
 module.exports = router;
